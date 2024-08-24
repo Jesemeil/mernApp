95fb2334da1c4c2f410ea139ba0096646c4188e9
Jesemeil <otahaghogho@gmail.com>
2024-08-24 20:51:34 +0100

    Updated Context Object

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -3,6 +3,27 @@ const mongoose = require("mongoose");
 
 const createWorkout = async (req, res) => {
   const { title, load, reps } = req.body;
+
+  let emptyFields = [];
+
+  if (!title) {
+    emptyFields.push("title");
+  }
+
+  if (!load) {
+    emptyFields.push("load");
+  }
+
+  if (!reps) {
+    emptyFields.push("reps");
+  }
+
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all fields", emptyFields });
+  }
+
   try {
     const workout = await Workout.create({ title, load, reps });
     res.status(200).json(workout);
@@ -46,7 +67,7 @@ const deleteWorkout = async (req, res) => {
     if (!workout) {
       return res.status(404).json({ error: "No such Workout" });
     }
-    res.status(200).json({ message: "deleted successfully" });
+    res.status(200).json(workout);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,19 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: `DELETE`,
+    });
+    const json = await response.json();
+    console.log("Deleted workout response:", json);
+    if (response.ok) {
+      dispatch({ type: `DELETE_WORKOUT`, payload: json });
+    }
+  };
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -10,7 +25,12 @@ const WorkoutDetails = ({ workout }) => {
         <strong>Number of reps:</strong>
         {workout.reps}
       </p>
-      <p>{workout.createdAt}</p>
+      <p>
+        {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+      </p>
+      <span className="material-symbols-outlined" onClick={handleClick}>
+        Delete
+      </span>
     </div>
   );
 };
